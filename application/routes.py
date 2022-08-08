2dbf3302914e210a608fa0cb267339167e3ab73c
Denys Postolnyi <postlden44@gmail.com>
2022-08-08 21:54:45 +0300

    created login, sessions

--- application/routes.py
+++ application/routes.py
@@ -1,42 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from application import app, db
 from application.models import Request
+from application import functions
 
 
 # from flask_restplus import Resource
-
-
-##################################################
-# API part
-
-
-##################################################
 
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home', methods=['POST', 'GET'])
 def home():
     message = None
+    if session.get('username'):
+        return render_template('index.html')
     if request.method == 'POST':
-        message = make_request(request.form)
+        if functions.make_request(request.form) == 'User saved':
+            message = f"{request.form.get('full_name')}, wellcome"
+            session['full_name'] = request.form.get('full_name')
+            session['email'] = request.form.get('email')
+        elif functions.make_request(request.form) == 'User founded':
+            message = f"{request.form.get('full_name')}, wellcome"
+            session['full_name'] = request.form.get('full_name')
+            session['email'] = request.form.get('email')
+        elif functions.make_request(request.form) == 'Email is already existed':
+            message = 'Email is already existed'
     return render_template('index.html', text=message)
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template('login.html')
+    if session.get('username'):
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        email = request.form.get('email')
+        full_name = request.form.get('full_name')
+        user = Request.objects(email=email).first()
+        if user and user['full_name'] == full_name:
+            session['full_name'] = full_name
+            session['email'] = email
+            return redirect(url_for("home"))
+    return redirect(url_for("home"))
 
 
-def make_request(form):
-    user_id = Request.objects.count() + 1
-    email = form.get('email')
-    email_from_db = Request.objects.filter(email=email)
-    if email_from_db:
-        return "email exist"
-    name = form.get('full_name')
-    plan = form.get('plan')
-    here = form.get('here')
+@app.route('/logout')
+def logout():
+    print('Enter')
+    print('#############################################')
+    session.pop('full_name', None)
+    print('ok')
+    session.pop('email', None)
+    print('ok')
+    return redirect(url_for('home'))
 
-    req = Request(user_id=user_id, full_name=name, email=email, plan=plan, here=here, is_active=True)
-    req.save()
-    return 'success'
+
+
